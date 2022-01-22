e42a1c511522fff20f8df0298dd4b27005df15dd
Arun <arunsingh641997@gmail.com>
2022-01-22 19:26:45 +0530

    score Added

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ function Start() {
         user.innerHTML = name;
     }   
 }
+function score(){
+    var uF = +document.getElementById("ufr").value;
+    var cF = +document.getElementById("cfr").value;
+
+    var winner = document.getElementById("win");
+
+
+    if(winner.innerHTML == "Tie"){
+        uF = uF+1;
+        cF=  cF+1;
+        document.getElementById("ufr").value = uF;
+        document.getElementById("cfr").value = cF;
+    }else if(winner.innerHTML == "User Wins"){
+        uF = uF+1;
+        document.getElementById("ufr").value = uF;
+
+    }else if(winner.innerHTML == "Computer Wins"){
+        cF=cF+1;
+        document.getElementById("cfr").value = cF;
+
+    }
+    
+}
+
 function play(){ 
     var arr=[0,1,2];
     var random;
@@ -31,6 +55,7 @@ function play(){
        }else if(comp.innerHTML == "Scissor" && user.innerHTML == "Rock" ){
             document.getElementById("win").innerHTML="Computer Wins";
        }
+       score();
 
 
     }function play2(){
@@ -43,6 +68,7 @@ function play(){
        }else if(comp.innerHTML == "Scissor" && user.innerHTML == "Paper" ){
             document.getElementById("win").innerHTML="Computer Wins";
        }
+       score();
 
     }function play3(){
         document.getElementById("user").innerHTML="Scissor";
@@ -54,5 +80,6 @@ function play(){
        }else if(comp.innerHTML == "Rock" && user.innerHTML == "Scissor" ){
             document.getElementById("win").innerHTML="Computer Wins";
        }
+       score();
 
     }
